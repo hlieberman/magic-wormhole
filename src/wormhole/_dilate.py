21ccb69cee686fde7e0d6cd0e682965bedeb36f2
Brian Warner <warner@lothar.com>
2018-01-02 08:03:26 +0100

    WIP

--- src/wormhole/_dilate.py
+++ src/wormhole/_dilate.py
@@ -1,5 +1,6 @@
 from attr import attrs, attrib
 from attr.validators import instance_of
+from twisted.internet.interfaces import ITransport, IProducer, IConsumer
 from automat import MethodicalMachine
 from nacl.secret import SecretBox
 from nacl.exceptions import CryptoError
@@ -165,6 +166,9 @@ class L3Connection(object):
         for (seqnum, msg) in self.outbound_queue:
             l2.sendMessage(msg)
 
+    def l2Closed(self):
+        self.l2 = None
+
     def ackReceived(self, inbound_seqnum):
         while self.outbound_queue.first()[0] <= inbound_seqnum: # ??
             self.outbound_queue.pop_first() # ??
@@ -177,6 +181,12 @@ class L3Connection(object):
 
     def closeReceived(self, seqnum, subchannel):
         pass
+
+    def send(self, seqnum, msg):
+        self.outbound_queue.append( (seqnum, msg) )
+        if self.l2:
+            self.l2.sendMessage(msg)
+
 
     # interface for the controlling side
     def openSubchannel(self):
@@ -192,22 +202,193 @@ class L3Connection(object):
         seqnum = self.seqnum()
         self.send(seqnum, make_close(seqnum, self._outbound_box, subchannel_id))
 
+    # interface for the L4 connection object
+    def sendData(self, subchannel_id, data):
+        seqnum = self.seqnum()
+        self.send(seqnum, make_data(seqnum, self._outbound_box, subchannel_id,
+                                    payload))
+    def sendClose(self, subchannel_id):
+        seqnum = self.seqnum()
+        self.send(seqnum, make_close(seqnum, self._outbound_box, subchannel_id))
+        # XXX remove from self._subchannels ?
+
+
 @attrs
+@implementer(_interfaces.ISubChannel)
+class SubChannel(object):
+    _id = attrib(validator=int)
+    _l3 = attrib(validator=instance_of(L3Connection))
+
+    m = MethodicalMachine()
+    set_trace = getattr(m, "_setTrace", lambda self, f: None)
+
+    def __attrs_post_init__(self):
+        #self._mailbox = None
+        #self._pending_outbound = {}
+        #self._processed = set()
+        pass
+
+    @m.state(initial=True)
+    def idle(self): pass # pragma: no cover
+
+    @m.state()
+    def open(self): pass # pragma: no cover
+
+    @m.state()
+    def closing(): pass # pragma: no cover
+
+    @m.state()
+    def closed(): pass # pragma: no cover
+
+
+    @m.input()
+    def remote_open(self): pass
+    @m.input()
+    def remote_data(self, data): pass
+    @m.input()
+    def local_data(self, data): pass
+    @m.input()
+    def local_close(self): pass
+    @m.input()
+    def remote_close(self): pass
+
+
+    @m.output()
+    def send_close(self):
+        self._l3.sendClose(self._id)
+
+    # things that can happen when idle
+    @m.output()
+    def error_early_remote_data(self, data):
+        raise Error
+    @m.output()
+    def error_early_local_data(self, data):
+        raise Error
+    @m.output()
+    def error_early_local_close(self):
+        raise Error
+    @m.output()
+    def error_early_remote_close(self):
+        raise Error
+
+
+    # things that can happen while open
+    @m.output()
+    def error_double_remote_open(self):
+        raise Error
+    @m.output()
+    def accept_data(self, data):
+        XXX
+    @m.output()
+    def send_data(self, data):
+        XXX
+    @m.output()
+    def send_close(self):
+        XXX
+    @m.output()
+    def send_close_and_close_subchannel(self):
+        XXX
+
+
+    # things that can happen while closing
+    @m.output()
+    def error_double_remote_open(self):
+        raise Error
+    @m.output()
+    def ignore_remote_data(self, data):
+        pass
+    @m.output()
+    def error_late_local_data(self, data):
+        raise Error
+    @m.output()
+    def error_double_local_close(self):
+        raise Error
+    @m.output()
+    def finished_closing(self):
+        XXX
+
+
+    # things that can happen while closed
+    @m.output()
+    def error_reopened(self):
+        raise Error
+    @m.output()
+    def error_late_remote_data(self, data):
+        raise Error
+    @m.output()
+    def error_late_local_data(self, data):
+        raise Error
+    @m.output()
+    def error_late_local_close(self):
+        raise Error
+    @m.output()
+    def error_late_remote_close(self):
+        raise Error
+
+    # primary transitions
+    idle.upon(remote_open, enter=open, outputs=[])
+    open.upon(remote_data, enter=open, outputs=[accept_data])
+    open.upon(local_data, enter=open, outputs=[send_data])
+    open.upon(remote_close, enter=closed, outputs=[send_close_and_close_subchannel])
+    open.upon(local_close, enter=closing, outputs=[send_close])
+    closing.upon(remote_close, enter=closed, outputs=[finished_closing])
+
+    # error cases
+    idle.upon(remote_data, enter=idle, outputs=[error_early_remote_data])
+    idle.upon(local_data, enter=idle, outputs=[error_early_local_data])
+    idle.upon(local_close, enter=idle, outputs=[error_early_local_close])
+    idle.upon(remote_close, enter=idle, outputs=[error_early_remote_close])
+    open.upon(remote_open, enter=open, outputs=[error_double_remote_open])
+    closing.upon(remote_open, enter=closing, outputs=[error_double_remote_open])
+    closing.upon(remote_data, enter=closing, outputs=[ignore_remote_data])
+    closing.upon(local_data, enter=closing, outputs=[error_late_local_data])
+    closing.upon(local_close, enter=closing, outputs=[error_double_local_close])
+    closed.upon(remote_open, enter=closed, outputs=[error_reopened])
+    closed.upon(remote_data, enter=closed, outputs=[error_late_remote_data])
+    closed.upon(local_data, enter=closed, outputs=[error_late_local_data])
+    closed.upon(local_close, enter=closed, outputs=[error_late_local_close])
+    closed.upon(remote_close, enter=closed, outputs=[error_late_remote_close])
+
+
+
+@attrs
+@implementer(ITransport)
 @implementer(IProducer)
 @implementer(IConsumer)
 class Subchannel(object):
     _l3 = attrib(validator=instance_of(L3Connection))
+    _l4 = attrib(validator=instance_of(SubChannel))
 
     def __attrs_post_init__(self):
         pass
 
+    def write(self, data):
+        self._l4.local_data(data)
+    def writeSequence(self, iovec):
+        self.write(b"".join(iovec))
+    def loseConnection(self):
+        self._l4.local_close()
+    def getPeer(self):
+        return None # XXX
+    def getHost(self):
+        return None # XXX
+
+    # IProducer
+    def stopProducing(self):
+        pass
     def pauseProducing(self):
         pass
     def resumeProducing(self):
         pass
 
-    def registerProducer(self, xyz):
+    # IConsumer
+    def registerProducer(self, producer, streaming):
+        # streaming==True: IPushProducer (pause/resume)
+        # streaming==False: IPullProducer (just resume)
         pass
+    def unregisterProducer(self):
+        pass
+
 
 @attrs
 @implementer(_interfaces.IDilatedConnection)
@@ -266,152 +447,3 @@ class DilatedConnection(object):
     connected.upon(send, enter=connected, outputs=[queue_and_send_message])
     connected.upon(receive_ack, enter=connected, outputs=[process_ack])
     connected.upon(connection_lost, enter=not_connected, outputs=[])
-
-from attr import attrs, attrib
-from attr.validators import instance_of
-from automat import MethodicalMachine
-from . import _interfaces
-
-@attrs
-
-@implementer(_interfaces.ISubChannel)
-class SubChannel(object):
-    _id = attrib(validator=int)
-    m = MethodicalMachine()
-    set_trace = getattr(m, "_setTrace", lambda self, f: None)
-
-    def __attrs_post_init__(self):
-        #self._mailbox = None
-        #self._pending_outbound = {}
-        #self._processed = set()
-        pass
-
-    @m.state(initial=True)
-    def idle(self): pass # pragma: no cover
-
-    @m.state()
-    def open(self): pass # pragma: no cover
-
-    @m.state()
-    def closing(): pass # pragma: no cover
-
-    @m.state()
-    def closed(): pass # pragma: no cover
-
-    @m.input()
-    def remote_open(self):
-        pass
-
-    @m.input()
-    def remote_data(self, x):
-        pass
-
-    @m.input()
-    def local_data(self, x):
-        pass
-
-    @m.input()
-    def local_close(self):
-        pass
-
-    @m.input()
-    def remote_close(self):
-        pass
-
-    @m.output()
-    def send_close(self):
-        pass
-
-    # things that can happen when idle
-    @m.output()
-    def error_early_remote_data(self, data):
-        raise Error
-    @m.output()
-    def error_early_local_data(self, data):
-        raise Error
-    @m.output()
-    def error_early_local_close(self):
-        raise Error
-    @m.output()
-    def error_early_remote_close(self):
-        raise Error
-
-    idle.upon(remote_open, enter=open, outputs=[])
-    idle.upon(remote_data, enter=idle, outputs=[error_early_remote_data])
-    idle.upon(local_data, enter=idle, outputs=[error_early_local_data])
-    idle.upon(local_close, enter=idle, outputs=[error_early_local_close])
-    idle.upon(remote_close, enter=idle, outputs=[error_early_remote_close])
-
-
-    # things that can happen while open
-    @m.output()
-    def error_double_remote_open(self):
-        raise Error
-    @m.output()
-    def accept_data(self, data):
-        XXX
-    @m.output()
-    def send_data(self, data):
-        XXX
-    @m.output()
-    def send_close(self):
-        XXX
-    @m.output()
-    def close_subchannel(self):
-        XXX
-
-    open.upon(remote_open, enter=open, outputs=[error_double_remote_open])
-    open.upon(remote_data, enter=open, outputs=[accept_data])
-    open.upon(local_data, enter=open, outputs=[send_data])
-    open.upon(local_close, enter=closing, outputs=[send_close])
-    open.upon(remote_close, enter=closed, outputs=[close_subchannel])
-
-
-    # things that can happen while closing
-    @m.output()
-    def error_double_remote_open(self):
-        raise Error
-    @m.output()
-    def ignore_remote_data(self, data):
-        pass
-    @m.output()
-    def error_late_local_data(self, data):
-        raise Error
-    @m.output()
-    def error_double_local_close(self):
-        raise Error
-    @m.output()
-    def finished_closing(self):
-        XXX
-
-    closing.upon(remote_open, enter=closing, outputs=[error_double_remote_open])
-    closing.upon(remote_data, enter=closing, outputs=[ignore_remote_data])
-    closing.upon(local_data, enter=closing, outputs=[error_late_local_data])
-    closing.upon(local_close, enter=closing, outputs=[error_double_local_close])
-    closing.upon(remote_close, enter=closed, outputs=[finished_closing])
-
-
-    # things that can happen while closed
-    @m.output()
-    def error_reopened(self):
-        raise Error
-    @m.output()
-    def error_late_remote_data(self, data):
-        raise Error
-    @m.output()
-    def error_late_local_data(self, data):
-        raise Error
-    @m.output()
-    def error_late_local_close(self):
-        raise Error
-    @m.output()
-    def error_late_remote_close(self):
-        raise Error
-
-    closed.upon(remote_open, enter=closed, outputs=[error_reopened])
-    closed.upon(remote_data, enter=closed, outputs=[error_late_remote_data])
-    closed.upon(local_data, enter=closed, outputs=[error_late_local_data])
-    closed.upon(local_close, enter=closed, outputs=[error_late_local_close])
-    closed.upon(remote_close, enter=closed, outputs=[error_late_remote_close])
-
-
